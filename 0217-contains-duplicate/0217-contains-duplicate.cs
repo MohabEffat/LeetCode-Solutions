28491de42f132862767648f6e6f23ffa9fcd4bcf
MohabEffat <99203754+MohabEffat@users.noreply.github.com>
2024-07-24 17:52:17 +0300

    Time: 253 ms (93.9%), Space: 67.8 MB (27.84%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cs
+++ 0217-contains-duplicate/0217-contains-duplicate.cs
@@ -1,17 +1,11 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-       Dictionary<int, int> dic = new Dictionary<int, int>();
-        bool flag = false;
-        for (int i = 0; i < nums.Length; i++)
+        HashSet<int> result = new HashSet<int>();
+        foreach (int i in nums)
         {
-            if (dic.ContainsKey(nums[i]))
-            {
-                flag = true;
-                break;
-            }
-            else
-                dic.Add(nums[i], 1);
+            if(!result.Add(i))
+                return true;
         }
-        return flag; 
+        return false;
     }
 }
